82b45076ec83176ef15297b6f6f1aef9676f0c58
darshit5modi <darshit5modi@gmail.com>
2023-09-28 22:09:58 +0530

    committed

---
+++ ApiGateway/src/test/java/com/lcwd/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,23 @@
+package com.lcwd.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}           
+
+}
+
+
+
+
+
+
+
